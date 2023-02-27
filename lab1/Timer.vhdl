5f2ffbd0083f8bbbff434c1b3d538661457df9b0
Séverin Konishi <konishi.seve@gmail.com>
2023-02-27 11:06:13 +0100

    timer compile

--- lab1/Timer.vhdl
+++ lab1/Timer.vhdl
@@ -15,75 +15,88 @@ entity Timer is
         address : in std_logic_vector(2 downto 0);
 
         -- interrupt
-        irq : out std_logic;
+        irq : out std_logic
     );
 end Timer;
 
 architecture comp of Timer is
     signal iRegCount : unsigned(31 downto 0);
-    signal iRegComp : std_logic_vector(31 downto 0);
+    signal iRegCompare : unsigned(31 downto 0);
     signal iRegEnable : std_logic;
-    signal iRegStatus : std_logic_vector(31 downto 0);
+    signal iRegEOT : std_logic;
     signal iRegIrqEnable : std_logic;
 
-    signal iClrEOF : std_logic;
+    signal iClrEOT : std_logic;
     signal iReset : std_logic;
-    signal 
 begin
 
 slave_write : process(clk,nReset)
 begin
     if nReset = '0' then
+        iRegEnable <= '0';
+        iReset <= '0';
+        iRegIrqEnable <= '0';
+        iRegCompare <= (others => '0');
 
     elsif rising_edge(clk) then
+        iReset <= '0';
+        iClrEOT <= '0';
         if write = '1' then
             case address is
-                when "000" => iRegCount <= 
-                when "001" => 
-
+                when "000" => null;
+                when "001" => iReset <= '1';
+                when "010" => iRegEnable <= writedata(0);
+                when "011" => iRegIrqEnable <= writedata(0);
+                when "100" => iClrEOT <= writedata(0);
+                when "101" => iRegCompare <= unsigned(writedata);
+                when others => null;
+            end case;
+        end if;
     end if;
 end process;
 
 
 slave_read : process(clk,nReset)
-
-
-
-
-
-
-
-
-
-
-process(clk,nReset)
-    begin
-    -- Reset
-    if nReset = '0' then
-        iRegCount <= (others => '0');
+begin
+    if rising_edge(clk) then
         readdata <= (others => '0');
-        iRegActive <= '0';
-    elsif rising_edge(clk) then
-        -- Avalon Slave read
         if read = '1' then
-            case Address is
-                when "00" => readdata <= iRegCount;
-                when others => null;
-            end case;
-        end if;
-        -- Avalon Slave write
-        if write = '1' then
             case address is
-                when "00" => iRegCount <= writedata;
-                when "01" => iRegCount <= (others => '0'); -- CLEAR
-                when "10" => iRegActive <= '1'; -- START
-                when "11" => iRegActive <= '0'; -- PAUSE
+                when "000" => readdata <= std_logic_vector(iRegCount);
+                when "011" => readdata(0) <= iRegIrqEnable;
+                when "100" => readdata(1) <= iRegEnable;
+                              readdata(0) <= iRegEOT;
+                when "101" => readdata <= std_logic_vector(iRegCompare);
                 when others => null;
             end case;
-        -- Counter incrementation
-        elsif iRegActive = '1' then
-            iRegCount <= std_logic_vector(unsigned(iRegCount) + 1);
         end if;
     end if;
 end process;
+
+
+count : process(clk,nReset)
+begin
+    if rising_edge(clk) then
+        if iReset = '1' then
+            iRegCount <= (others => '0');
+        elsif iRegEnable = '1' then
+            iRegCount <= iRegCount + 1;
+        end if;
+    end if;
+end process;
+
+
+interrupt : process(clk,nReset)
+begin
+    if rising_edge(clk) then
+        if iRegCount = iRegCompare then
+            iRegEOT <= '1';
+        elsif iClrEOT = '1' then
+            iRegEOT <= '0';
+        end if;
+    end if;
+end process;
+
+irq <= '1' when iRegEOT = '1' and iRegIrqEnable = '1' and iRegEnable = '1' else '0';
+
 end comp;
